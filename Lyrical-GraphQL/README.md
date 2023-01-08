04faf24900865fe4a0976d009bdf206f7b451e40
space-schnoodle <space.schnoodle@gmail.com>
2023-01-08 10:51:29 -0500

    finish lyrical project

--- Lyrical-GraphQL/README.md
+++ Lyrical-GraphQL/README.md
@@ -1,2 +1,10 @@
 # Lyrical-GraphQL
+
 Starter project from a GraphQL course on Udemy.com
+
+## To test it
+
+- Go to the root of the project
+- Install the dependencies
+- Make sure you have an update moongose package
+- Run `npm start dev`
--- Lyrical-GraphQL/client/index.js
+++ Lyrical-GraphQL/client/index.js
@@ -1,3 +1,4 @@
+import "./style/style.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Router, Route, hashHistory, IndexRoute } from "react-router";
@@ -6,9 +7,12 @@ import { ApolloProvider } from "react-apollo";
 
 import SongList from "./components/SongList";
 import SongCreate from "./components/SongCreate";
+import SongDetail from "./components/SongDetail";
 import App from "./components/App";
 
-const client = new ApolloClient({});
+const client = new ApolloClient({
+  dataIdFromObject: (o) => o.id,
+});
 
 const Root = () => {
   return (
@@ -17,6 +21,7 @@ const Root = () => {
         <Route path="/" component={App}>
           <IndexRoute component={SongList} />
           <Route path="songs/new" component={SongCreate} />
+          <Route path="songs/:id" component={SongDetail} />
         </Route>
       </Router>
     </ApolloProvider>
